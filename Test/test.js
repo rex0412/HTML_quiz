b2513875872a68f4be800cda777dfe0076d05b73
DaeYong_0412 <84562268+rex0412@users.noreply.github.com>
2022-05-16 15:15:49 +0900

    .

---
+++ Test/test.js
@@ -0,0 +1,13 @@
+const title = document.querySelector(".h1-wrapper h1");
+
+title.innerHTML = "수업 끝났다~~~~~!"
+
+
+
+// console.log(document.querySelector(".h1-wrapper h1"));
+
+// console.log(document.querySelector("#wrapper-id h1"));
+
+// const h1_list = document.querySelectorAll("h1");
+
+// console.log(h1_list[2]);
