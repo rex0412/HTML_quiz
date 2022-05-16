8254172403dc1e377635a71a5c48e466d803b759
DaeYong_0412 <84562268+rex0412@users.noreply.github.com>
2022-05-16 12:13:18 +0900

    js text

---
+++ Text/app.js
@@ -0,0 +1,27 @@
+
+// sayHello("yeji");
+
+// sayHello("jibok");
+
+// sayHello("mdol");
+
+// const sayHello = function (name) {
+//     console.log("Hello");
+//     console.log("Hi");
+//     console.log(name);
+// }
+
+// function sayHello(name) {
+//     console.log("Hello");
+//     console.log("Hi");
+//     console.log(name);
+// }
+
+// alert('알람!');
+
+// const age  = prompt("나이를 입력하세요");
+// console.log(age);
+// console.log(Number(age))
+
+const msg = confirm('로그아웃 하시겠습니까?');
+console.log(msg);
