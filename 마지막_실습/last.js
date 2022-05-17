ee8ba2768721b8a04884826db933c130f0e12c6a
DaeYong_0412 <84562268+rex0412@users.noreply.github.com>
2022-05-17 13:53:15 +0900

    로그인 ID 입력창

--- 마지막_실습/last.js
+++ 마지막_실습/last.js
@@ -1,16 +1,23 @@
 const header_h1 = document.querySelector("header h1")
 
-function h1_clicked() {
-    console.log("h1이 클릭되었습니다!");
+function h1ClickHandler() {
+    console.log("H1 요소를 클릭했네요!");
+    alert("잘 실행됩니다!");
 }
 
-header_h1.addEventListener("click", h1_clicked);
+header_h1.addEventListener("click", h1ClickHandler);
 
 const menu_login = document.querySelector("#login-link");
 
 function loginAlert() {
 
-    alert('로그인하세요!')
+    
 }
 
-menu_login.addEventListener("click", loginAlert)
+
+function askId() {
+    const user_id = prompt("ID값을 입력해주세요");
+    console.log(user_id);
+}
+const login_menu = document.querySelector("#login-link");
+login_menu.addEventListener("click", askId);
